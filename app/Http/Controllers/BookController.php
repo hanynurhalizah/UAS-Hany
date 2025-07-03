e3ad3dd75ec77603c71789aa5206028572dd879e
Hany Nurhalizah <hanynurhalizah22@gmail.com>
2025-07-03 10:21:21 +0700

    membuat route-resource beserta book controller

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    // ...controller methods (index, create, store, show, edit, update, destroy) can be filled as needed
+}
